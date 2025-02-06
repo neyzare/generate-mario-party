9aa135345865bf600ef68f0ec2fa7b32483e6861
neyzar <lucas.narguet451@gmail.com>
2025-02-06 11:36:32 +0100

    fix bug

--- App.js
+++ App.js
@@ -1,31 +1,80 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { BlurView } from "expo-blur"; 
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View, Animated, Easing, ActivityIndicator, Image } from "react-native";
+import { BlurView } from "expo-blur";
+import { useEffect, useRef, useState } from "react";
 import { useStore } from "./store/useStore";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  const { selectedImage, selectedIcon, getRandomBoard } = useStore(); 
+  const { selectedImage, selectedIcon, getRandomBoard, logo } = useStore();
+  
+
+
+  const [isFontLoaded] = useFonts({
+    "maPolice": require("./assets/fonts/AOTFShinGoProDeBold.otf"), 
+  });
+
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (isFontLoaded) {
+      setIsReady(true);
+    }
+  }, [isFontLoaded]);
+
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(animatedValue, {
+          toValue: -10,
+          duration: 800,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }),
+        Animated.timing(animatedValue, {
+          toValue: 10,
+          duration: 800,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, []);
 
   return (
-    <ImageBackground source={selectedImage} style={styles.background}>
-
-      <BlurView intensity={9} style={styles.blurContainer}>
-        <Text style={styles.title}>Mario Partie</Text>
-
-   
-        <Image source={selectedIcon} style={styles.icon} />
-
-        <TouchableOpacity onPress={getRandomBoard} style={styles.button}>
-          <Text style={styles.buttonText}>Générer une map</Text>
-        </TouchableOpacity>
-      </BlurView>
-
-      <StatusBar style="auto" />
-    </ImageBackground>
+    <View style={styles.container}>
+      {!isReady ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      ) : (
+        <ImageBackground source={selectedImage} style={styles.background}>
+          <BlurView intensity={9} style={styles.blurContainer}>
+           
+            <Image source={logo} style={styles.logo} />
+            
+            <Animated.Image
+              source={selectedIcon}
+              style={[styles.icon, { transform: [{ translateY: animatedValue }] }]}
+            />
+            <TouchableOpacity onPress={getRandomBoard} style={styles.button}>
+              <Text style={styles.buttonText}>Générer une map</Text>
+            </TouchableOpacity>
+          </BlurView>
+          <StatusBar style="auto" />
+        </ImageBackground>
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
   background: {
     flex: 1,
     resizeMode: "cover",
@@ -34,18 +83,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "rgba(255, 255, 255, 0.2)", 
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
     padding: 20,
   },
-  title: {
-    fontSize: 22,
-    fontWeight: "bold",
-    marginBottom: 20,
+  logo: {
+    width: 290,
+    height: 290,
+    paddingBottom: 90,
+    resizeMode: "contain",
   },
   icon: {
-    width: 150,
-    height: 150,
+    width: 300,
+    height: 300,
+    borderRadius: 9999,
+    overflow: "hidden",
     marginBottom: 10,
+    resizeMode: "contain",
   },
   button: {
     marginTop: 20,
@@ -56,5 +109,11 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     fontSize: 16,
+    fontFamily: "maPolice",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- store/useStore.js
+++ store/useStore.js
@@ -2,13 +2,14 @@ import { create } from "zustand";
 import { data } from "./data"; 
 
 export const useStore = create((set) => ({
+  logo: require("../assets/Super_Mario_Party_Jamboree_Logo.png"),
   selectedImage: require("../assets/super-mario-party-jamboree-website-board-1.jpg"), 
   selectedIcon: require("../assets/super-mario-party-jamboree-board-icon-1.png"),
   getRandomBoard: () => {
     const randomBoard = data[Math.floor(Math.random() * data.length)];
     set({ 
       selectedImage: randomBoard.boardView,
-      selectedIcon: randomBoard.boardIcon
+      selectedIcon: randomBoard.boardIcon,
     });
   },
 }));
